ab9c5ff67a8682ff2db983b7ce45e852e6af5841
iTi-Tien <144105196+iti-tien@users.noreply.github.com>
2023-09-05 18:13:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Website</title>
+</head>
+<body>
+    <?php
+    // Mã PHP bắt đầu ở đây
+    echo "<h1>Hello, World!</h1>";
+    echo "<p>This is a basic PHP web page.</p>";
+    // Mã PHP kết thúc ở đây
+    ?>
+</body>
+</html>
+
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
