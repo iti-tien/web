aa431471dd1d231fc17a3a6c5d19166ae0e91767
iTi-Tien <144105196+iti-tien@users.noreply.github.com>
2023-09-06 10:12:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,34 @@ document.addEventListener("DOMContentLoaded", function () {
     const taskForm = document.getElementById("task-form");
     const taskInput = document.getElementById("task-input");
 
+    // Lấy danh sách công việc từ Local Storage khi trang web tải lại
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    // Hiển thị danh sách công việc đã lưu
+    savedTasks.forEach(taskText => {
+        const taskItem = document.createElement("li");
+        taskItem.innerHTML = `
+            <span>${taskText}</span>
+            <button class="delete">Xóa</button>
+            <button class="done">Done</button>
+        `;
+
+        taskList.appendChild(taskItem);
+
+        const deleteButton = taskItem.querySelector(".delete");
+        deleteButton.addEventListener("click", function () {
+            taskList.removeChild(taskItem);
+            updateLocalStorage();
+        });
+
+        const doneButton = taskItem.querySelector(".done");
+        doneButton.addEventListener("click", function () {
+            taskItem.classList.toggle("done");
+            updateLocalStorage();
+        });
+    });
+
+    // Xử lý sự kiện khi người dùng thêm công việc
     taskForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
@@ -23,11 +51,22 @@ document.addEventListener("DOMContentLoaded", function () {
         const deleteButton = taskItem.querySelector(".delete");
         deleteButton.addEventListener("click", function () {
             taskList.removeChild(taskItem);
+            updateLocalStorage();
         });
 
         const doneButton = taskItem.querySelector(".done");
         doneButton.addEventListener("click", function () {
             taskItem.classList.toggle("done");
+            updateLocalStorage();
         });
+
+        // Lưu công việc mới vào Local Storage
+        savedTasks.push(taskText);
+        updateLocalStorage();
     });
+
+    // Hàm cập nhật Local Storage
+    function updateLocalStorage() {
+        localStorage.setItem("tasks", JSON.stringify(savedTasks));
+    }
 });
